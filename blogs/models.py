19413cda4f39a9190be2774e90b1611482cc21b0
Moopz-Dev <pongzthor@gmail.com>
2022-04-17 12:28:35 +0700

    feat: adds blog model and migrations

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from category.models import Category
 
 # Create your models here.
+
+
+class Blog(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    content = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    writer = models.CharField(max_length=255)
+    views = models.IntegerField(default=0)
+    images = models.ImageField(upload_to="blogImages", blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
