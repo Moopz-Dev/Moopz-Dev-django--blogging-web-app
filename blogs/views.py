bca746610aee89fa5c1029cec68f33088c9dfeb9
Moopz-Dev <pongzthor@gmail.com>
2022-04-17 19:36:48 +0700

    feat : adds serachWriter, searchCategory, orderByView

--- blogs/views.py
+++ blogs/views.py
@@ -10,6 +10,13 @@ def index(request):
     categories = Category.objects.all()
     blogs = Blog.objects.all()
     latest = Blog.objects.all().order_by('-pk')[:4]
+
+    # Popular posts
+    popular = Blog.objects.all().order_by('-views')[:3]
+
+    # Recommended posts
+    suggestion = Blog.objects.all().order_by('views')[:3]
+
     # pagination
     paginator = Paginator(blogs, 3)
     try:
@@ -20,11 +27,32 @@ def index(request):
         blogPerPage = paginator.page(page)
     except(EmptyPage, InvalidPage):
         blogPerPage = paginator.page(paginator.num_pages)
-    return render(request, "frontend/index.html", {'categories': categories, "blogs": blogPerPage, "latest": latest})
+    return render(request, "frontend/index.html", {'categories': categories, "blogs": blogPerPage, "latest": latest, "popular": popular, "suggestion": suggestion})
 
 
 def blogDetail(request, id):
+    categories = Category.objects.all()
+    popular = Blog.objects.all().order_by('-views')[:3]
+    suggestion = Blog.objects.all().order_by('views')[:3]
+
     singleBlog = Blog.objects.get(id=id)
     singleBlog.views = singleBlog.views + 1
     singleBlog.save()
-    return render(request, "frontend/blogDetail.html", {"blog": singleBlog})
+    return render(request, "frontend/blogDetail.html", {"blog": singleBlog, "categories": categories, "popular": popular, "suggestion": suggestion})
+
+
+def searchCategory(request, category_id):
+    categoryName = Category.objects.get(id=category_id)
+    categories = Category.objects.all()
+    blogs = Blog.objects.filter(category_id=category_id)
+    popular = Blog.objects.all().order_by('-views')[:3]
+    suggestion = Blog.objects.all().order_by('views')[:3]
+    return render(request, "frontend/searchCategory.html", {"blogs": blogs, "categories": categories, "popular": popular, "suggestion": suggestion, "categoryName": categoryName})
+
+
+def searchWriter(request, writer):
+    categories = Category.objects.all()
+    blogs = Blog.objects.filter(writer=writer)
+    popular = Blog.objects.all().order_by('-views')[:3]
+    suggestion = Blog.objects.all().order_by('views')[:3]
+    return render(request, "frontend/searchWriter.html", {"writerName": writer, "categories": categories, "blogs": blogs, "popular": popular, "suggestion": suggestion})
