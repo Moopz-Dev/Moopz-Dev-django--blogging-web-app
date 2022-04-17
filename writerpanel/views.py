81bf97b87d547953d35dc45a8d764ce9114e543d
Moopz-Dev <pongzthor@gmail.com>
2022-04-17 22:28:47 +0700

    feat : renders dashboard dynamically

--- writerpanel/views.py
+++ writerpanel/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-
+from blogs.models import Blog
+from django.db.models import Sum
 # Create your views here.
 
 
 def panel(request):
-    return render(request, "backend/index.html")
+    writer = "moopz"
+    blogs = Blog.objects.filter(writer=writer)
+    blogCount = blogs.count()
+    total = blogs.aggregate(Sum("views"))
+    return render(request, "backend/index.html", {"blogs": blogs, "writer": writer, "blogCount": blogCount, "total": total})
