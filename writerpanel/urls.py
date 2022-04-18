206f97d75e39b7f648f504f63ac3aac7bdc12800
Moopz-Dev <pongzthor@gmail.com>
2022-04-18 22:00:13 +0700

    feat : adds blogform page

--- writerpanel/urls.py
+++ writerpanel/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import panel
+from .views import panel, displayForm
 
 urlpatterns = [
-    path('', panel, name='panel')
+    path('', panel, name='panel'),
+    path('displayForm', displayForm, name="displayForm")
 ]
--- writerpanel/views.py
+++ writerpanel/views.py
@@ -3,6 +3,7 @@ from blogs.models import Blog
 from django.db.models import Sum
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import auth
+from category.models import Category
 # Create your views here.
 
 
@@ -13,3 +14,12 @@ def panel(request):
     blogCount = blogs.count()
     total = blogs.aggregate(Sum("views"))
     return render(request, "backend/index.html", {"blogs": blogs, "writer": writer, "blogCount": blogCount, "total": total})
+
+
+def displayForm(request):
+    writer = auth.get_user(request)
+    blogs = Blog.objects.filter(writer=writer)
+    blogCount = blogs.count()
+    total = blogs.aggregate(Sum("views"))
+    categories = Category.objects.all()
+    return render(request, 'backend/blogForm.html', {"blogs": blogs, "writer": writer, "blogCount": blogCount, "total": total, 'categories': categories})
