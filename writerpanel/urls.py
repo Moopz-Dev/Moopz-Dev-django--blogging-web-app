05f9d9566a3109ad2197ccba08d012d7172e1f53
Moopz-Dev <pongzthor@gmail.com>
2022-04-19 21:56:12 +0700

    feat : implement blogForm functionalities

--- writerpanel/urls.py
+++ writerpanel/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import panel, displayForm
+from numpy import insert
+from .views import panel, displayForm, insertData
 
 urlpatterns = [
     path('', panel, name='panel'),
-    path('displayForm', displayForm, name="displayForm")
+    path('displayForm', displayForm, name="displayForm"),
+    path('insertData', insertData, name="insertData")
 ]
--- writerpanel/views.py
+++ writerpanel/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+import re
+from django.shortcuts import redirect, render
 from blogs.models import Blog
 from django.db.models import Sum
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import auth
 from category.models import Category
+from django.core.files.storage import FileSystemStorage
+from django.contrib import messages
 # Create your views here.
 
 
@@ -23,3 +26,26 @@ def displayForm(request):
     total = blogs.aggregate(Sum("views"))
     categories = Category.objects.all()
     return render(request, 'backend/blogForm.html', {"blogs": blogs, "writer": writer, "blogCount": blogCount, "total": total, 'categories': categories})
+
+
+def insertData(request):
+    if request.method == "POST" and request.FILES["image"]:
+        datafile = request.FILES["image"]
+        name = request.POST["name"]
+        category = request.POST["category"]
+        description = request.POST["description"]
+        content = request.POST["content"]
+        writer = auth.get_user(request)
+
+        if str(datafile.content_type).startswith("image"):
+            fs = FileSystemStorage()
+            img_url = "blogImages/"+datafile.name
+            filename = fs.save(img_url, datafile)
+            blog = Blog(name=name, description=description,
+                        category_id=category, content=content, writer=writer, image=img_url)
+            blog.save()
+            messages.info(request, "Data Successfully Written!")
+            return redirect("displayForm")
+        else:
+            messages.info(request, "Invalid file type, please reupload!")
+            return redirect("displayForm")
